5066061b61010ff502c7fa1b6fac7a643a4b31ea
Tempestmon <misha8936@yandex.ru>
2024-12-15 14:13:01 +0500

    feat: add swagger for api

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::web::Json;
 use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use models::Message;
-use utoipa;
+use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
 mod models;
@@ -11,7 +11,8 @@ mod models;
     path = "/message",
     request_body = Message,
     responses(
-        (status = 200, description = "Message sent successfully", body = Message)
+        (status = 200, description = "Message sent successfully", body = Message),
+        (status = 400, description = "Invalid payload"),
     )
 )]
 #[post("/message")]
@@ -20,19 +21,38 @@ async fn send_message(message: Json<Message>) -> impl Responder {
     HttpResponse::Ok().json(message)
 }
 
+#[utoipa::path(
+    get,
+    path = "/messages",
+    responses(
+        (status = 200, description = "Nothing yet")
+    )
+)]
 #[get("/messages")]
 async fn get_messages() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(OpenApi)]
+#[openapi(
+    paths(send_message, get_messages),
+    components(schemas(Message)),
+    tags(
+        (name = "Messaging", description = "API for messaging operations")
+    )
+)]
+struct ApiDoc;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let openapi = ApiDoc::openapi();
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
             .service(send_message)
             .service(get_messages)
-            .service(SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", openapi))
+            .service(
+                SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-docs/openapi.json", openapi.clone()),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
