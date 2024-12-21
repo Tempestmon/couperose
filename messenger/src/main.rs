cf4a147ce52b314e59e8711bed031e5a6f070fb1
Tempestmon <misha8936@yandex.ru>
2024-12-21 20:57:43 +0500

    fix: split method from main

--- messenger/src/main.rs
+++ messenger/src/main.rs
@@ -1,52 +1,22 @@
 use tonic::transport::Server;
 
-use proto::{
-    messenger_server::{Messenger, MessengerServer},
-    SendMessageResponse,
-};
+use proto::messenger_server::MessengerServer;
+
+
+mod methods;
 
 mod proto {
     tonic::include_proto!("messenger");
-}
-
-#[derive(Debug, Default)]
-struct MessengerService {}
-
-#[tonic::async_trait]
-impl Messenger for MessengerService {
-    async fn send_message(
-        &self,
-        request: tonic::Request<proto::SendMessageRequest>,
-    ) -> std::result::Result<tonic::Response<proto::SendMessageResponse>, tonic::Status> {
-        let input = request.get_ref();
-
-        print!("Got request from {}", input.sender);
-
-        let response = SendMessageResponse { success: true };
-
-        Ok(tonic::Response::new(response))
-    }
-
-    async fn get_messages(
-        &self,
-        _request: tonic::Request<proto::GetMessagesRequest>,
-    ) -> std::result::Result<tonic::Response<proto::GetMessagesResponse>, tonic::Status> {
-        todo!()
-    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
 
-    let messenger = MessengerService::default();
-
-    let service = tonic_reflection::server::Builder::configure()
-        .build_v1()?;
+    let messenger = methods::MessengerService::default();
 
     Server::builder()
         .add_service(MessengerServer::new(messenger))
-        .add_service(service)
         .serve(addr)
         .await?;
 
