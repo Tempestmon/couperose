49fe84168bc8ab5ad1caeb35dbcaa53c74f23312
Tempestmon <misha8936@yandex.ru>
2025-03-14 21:54:43 +0500

    fix(soryu): add dir

--- docker/start-nginx.sh
+++ docker/start-nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p /etc/nginx/sites-enabled
+
 envsubst '${API_TOKEN}' </etc/nginx/templates/messenger.conf >/etc/nginx/sites-enabled/messenger.conf
 
 exec nginx -g "daemon off;"
