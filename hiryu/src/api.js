a751eabd1b23f30385119be6785e1c07bd393d92
Tempestmon <misha8936@yandex.ru>
2025-03-16 22:44:08 +0500

    fix(hiryu): hardcode api url

--- hiryu/src/api.js
+++ hiryu/src/api.js
@@ -1,33 +1,34 @@
-const API_BASE = __API_URL__;
+// const API_BASE = __API_URL__;
+const API_BASE = "https://messenger.tempestmon.ru/api:8080";
 
 export async function fetchMessages() {
-    console.log(`From local: ${API_BASE}`);
-    const response = await fetch(`${API_BASE}/messages`);
-    if (!response.ok) throw new Error('Failed to fetch messages');
-    console.log(response);
-    return response.json();
+  console.log(`From local: ${API_BASE}`);
+  const response = await fetch(`${API_BASE}/messages`);
+  if (!response.ok) throw new Error('Failed to fetch messages');
+  console.log(response);
+  return response.json();
 }
 
 export async function sendMessage(sender, content) {
-    console.log('Sending request to API:', {
-        sender,
-        recipient: 'Everyone',
-        content,
-    });
-    const response = await fetch(`${API_BASE}/message`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            sender,
-            recipient: 'Everyone',
-            content,
-        }),
-    });
+  console.log('Sending request to API:', {
+    sender,
+    recipient: 'Everyone',
+    content,
+  });
+  const response = await fetch(`${API_BASE}/message`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      sender,
+      recipient: 'Everyone',
+      content,
+    }),
+  });
 
-    if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(`HTTP ${response.status}: ${errorText}`);
-    }
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`HTTP ${response.status}: ${errorText}`);
+  }
 }
